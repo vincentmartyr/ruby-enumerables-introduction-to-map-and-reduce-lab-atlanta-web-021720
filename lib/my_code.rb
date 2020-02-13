fdb02d8758e1c546eab79441f95b0c8fc30c65b4
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-13 02:04:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -16,10 +16,11 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
-  (source_array).reduce(starting_point, :+)
+  [source_array].reduce(starting_point, :+)
 end
 
 def reduce_to_all_true(source_array)
+  ()
 end
 
 def reduce_to_any_true(source_array)
