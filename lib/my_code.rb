96571ab8061f988c0ed04db7b833de7d63483bec
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-13 03:03:26 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
-# My Code here....
+# My Code here...
+def map_to_negativize(source_array)
+  source_array.map { |e| e * -1  }
+end
+
+def map_to_no_change(source_array)
+  source_array.map { |e| e*1 }
+end
+
+def map_to_double(source_array)
+  source_array.map { |e| e*2 }
+end
+
+def map_to_square(source_array)
+  source_array.map { |e| e*e }
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+  source_array.reduce(starting_point , :+)
+end
+
+def reduce_to_all_true(source_array)
+  source_array.all?
+end
+
+def reduce_to_any_true(source_array)
+  source_array.any?
+end
