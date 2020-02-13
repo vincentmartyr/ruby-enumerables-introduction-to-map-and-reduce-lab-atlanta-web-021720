4eaf575d3686b784a2036db065d9a8ebd7e1f4e1
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-13 02:34:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,7 +20,7 @@ def reduce_to_total(source_array, starting_point)
 end
 
 def reduce_to_all_true(source_array)
-  source_array.reduce(all?, :true)
+  source_array.all?,
 end
 
 def reduce_to_any_true(source_array)
